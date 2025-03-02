fad90f3507abde4f1a0a504adc9f59e227f77db1
devknightingale <krola20@wgu.edu>
2025-03-01 20:53:29 -0500

    User Schedule & Monthly Appointments reports completed

--- C969 - Scheduler/Entity Classes/Helper.cs
+++ C969 - Scheduler/Entity Classes/Helper.cs
@@ -27,6 +27,7 @@ namespace C969___Scheduler.Entity_Classes
         public static int customerIdValue {  get; set; }
 
         public static List<DateTime> boldedSelection = new List<DateTime>();
+        
         /**********************/
         /***** DATA  GRID *****/
         /**********************/
@@ -34,11 +35,11 @@ namespace C969___Scheduler.Entity_Classes
         {
             return TimeZone.CurrentTimeZone.StandardName;
         }
-        public static int GetUserID()
+        public static int GetUserID(string name)
         {
             MySqlCommand cmd = DBConnection.conn.CreateCommand();
             cmd.CommandText = "SELECT userId FROM user WHERE userName = @userName";
-            cmd.Parameters.AddWithValue("@userName", userNameValue);
+            cmd.Parameters.AddWithValue("@userName", name);
             userIdValue = (int)cmd.ExecuteScalar();
 
             return userIdValue;
@@ -639,27 +640,58 @@ namespace C969___Scheduler.Entity_Classes
             }
         }
 
-        public static List<Appointment> GetSchedule()
+        public static List<Appointment> GetSchedule(string name)
         {
-            int userId = GetUserID();
+            int userId = GetUserID(name);
 
             List<Appointment> apptList = new List<Appointment>();
             MySqlCommand cmd = DBConnection.conn.CreateCommand();
-            cmd.CommandText = "SELECT customer.customerName as 'Customer Name', appointment.title as 'Title', appointment.start as 'Start' FROM appointment  LEFT JOIN customer ON customer.customerId = appointment.customerId WHERE appointment.userId = @userId";
+            cmd.CommandText = "SELECT customer.customerName as 'Customer Name', appointment.title as 'Title', appointment.start as 'Start', appointment.type as 'Type' FROM appointment  LEFT JOIN customer ON customer.customerId = appointment.customerId WHERE appointment.userId = @userId";
             cmd.Parameters.AddWithValue("@userId", userId);
             MySqlDataReader reader = cmd.ExecuteReader();
             while (reader.Read())
             {
 
-                apptList.Add(new Appointment() {
-                    start = Convert.ToDateTime(reader["start"])
-                });
+                apptList.Add(new Appointment()
+                {
+                    start = Convert.ToDateTime(reader["start"]),
+                    type = reader["type"].ToString(),
+                    title = reader["title"].ToString()
+                }); 
 
             }
             reader.Close();
             return apptList;
             
 
+        }
+        public static List<Appointment> GetMonthlyAppointments(string month, ReportForm form)
+        {
+            List<Appointment> monthlyAppts = new List<Appointment>();
+            
+            int endDay = DateTime.DaysInMonth(DateTime.Now.Year, form.cbMonths.SelectedIndex + 1);
+            DateTime startOfMonth = DateTime.Parse(month + " 1," + " " + DateTime.Now.Year).ToUniversalTime();
+            DateTime endOfMonth = DateTime.Parse(month + " " + endDay + ", " + DateTime.Now.Year + " 23:59:59").ToUniversalTime();
+
+
+            MySqlCommand cmd = DBConnection.conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM appointment WHERE start > @firstOfMonth AND start < @endOfMonth";
+            cmd.Parameters.AddWithValue("@firstOfMonth", startOfMonth.ToString("yyyy-MM-dd hh:mm:ss"));
+            cmd.Parameters.AddWithValue("@endOfMonth", endOfMonth.ToString("yyyy-MM-dd hh:mm:ss"));
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                monthlyAppts.Add(new Appointment()
+                {
+                    start = Convert.ToDateTime(reader["start"]),
+                    type = reader["type"].ToString(),
+                    title = reader["title"].ToString(),
+
+                }); 
+            }
+            reader.Close(); 
+            return monthlyAppts;
         }
     }
 
--- C969 - Scheduler/ReportForm.cs
+++ C969 - Scheduler/ReportForm.cs
@@ -6,11 +6,14 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace C969___Scheduler
 {
@@ -19,29 +22,90 @@ namespace C969___Scheduler
         public ReportForm(string user)
         {
             InitializeComponent();
+
+            // sets user for the user schedule function 
+            List<string> users = new List<string>();            
+            MySqlCommand cmd = DBConnection.conn.CreateCommand();
+            cmd.CommandText = "SELECT userName FROM user"; 
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                cbUsers.Items.Add(reader["userName"].ToString());
+            }
+            reader.Close();
+
+
+            // sets combobox for months 
+            string[] months = new string[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
             
+            foreach (string month in months)
+            {
+                cbMonths.Items.Add(month); 
+            }
+
+            // sets combobox for customers 
+            MySqlCommand cmdCust = DBConnection.conn.CreateCommand();
+            cmdCust.CommandText = "SELECT customerName FROM customer";
+            MySqlDataReader rdrCust = cmdCust.ExecuteReader(); 
+            while (rdrCust.Read())
+            {
+                cbCustomers.Items.Add(rdrCust["customerName"].ToString());
+            }
+            rdrCust.Close(); 
         }
 
-        private void btnMonthlyApptsReport_Click(object sender, EventArgs e)
+        private void btnUserScheduleReport_Click(object sender, EventArgs e)
         {
-            List<Appointment> userAppointments = Helper.GetSchedule();
-            string reportString = $"{Helper.userNameValue} Appointments:\n\n";
-
-            // lambda to simplify foreach statement
-            userAppointments.ForEach(appt => 
+            if (cbUsers.SelectedItem == null)
             {
-                reportString += "\n\nStart: " + appt.start + Environment.NewLine;
+                MessageBox.Show("You must select a user first.");
+            }
+            else
+            {
+                string name = cbUsers.SelectedItem.ToString();
+                List<Appointment> userAppointments = Helper.GetSchedule(name);
 
-            });
-            txtReport.Text = reportString;
+                string reportString = $"{name} Appointments:" + Environment.NewLine + Environment.NewLine;
+
+                // lambda to simplify foreach statement
+                userAppointments.ForEach(appt =>
+                {
+                    reportString += "\n\nStart: " + appt.start + "  Type: " + appt.type + "  Title: " + appt.title + Environment.NewLine;
+
+                });
+                txtReport.Text = reportString;
+            }
+            
 
         }
 
         private void btnAppointmentType_Click(object sender, EventArgs e)
         {
-            
+            if (cbMonths.SelectedItem == null)
+            {
+                MessageBox.Show("You must select a month first.");
+            }
+            else
+            {
+                // take in month, this? 
+                string month = cbMonths.SelectedItem.ToString();
+                List<Appointment> monthlyAppts = Helper.GetMonthlyAppointments(month, this);
+
+                string reportString = $"{month} Appointments:" + Environment.NewLine + Environment.NewLine;
+
+                // lambda to simplify foreach statement
+                monthlyAppts.ForEach(appt =>
+                {
+                    reportString += "\n\nStart: " + appt.start + "  Type: " + appt.type + "  Title: " + appt.title + Environment.NewLine;
+
+                });
+                txtReport.Text = reportString;
+
+            }
 
             
         }
+
+        
     }
 }
